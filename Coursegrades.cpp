64fb4b66cfea8acb4a245f9efdc885279f4b22ea
Charles Sherwood <kulan876321@yahoo.com>
2026-03-03 10:19:31 -0500

    Add project files.

---
+++ Coursegrades.cpp
@@ -0,0 +1,16 @@
+/*FileName:Coursegrades.cpp
+Programmer Name:Charles Sherwood 
+Date:03/03/2026
+Requirements:This program should reasds grade data from
+a file and dynamically allocates the memory to store student records and test
+scores calculates the averages and assigns letter grades and 
+output a formatted course grade report
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
